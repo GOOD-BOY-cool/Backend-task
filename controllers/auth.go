53f3a1cbd577f2a13a10ec93c0f7fd04f8e6548f
GOOD-BOY-cool <zjzxh123.zj@outlook.com>
2026-09-13 22:29:22 +0800

    3

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,1 +1,26 @@
 package controllers
+
+import (
+	"backend/utils"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB(db *gorm.DB) {
+	DB = db
+}
+func Register(c *gin.Context) {
+	var body struct {
+		Account  string `json:"account"`
+		Password string `json:"password"`
+		QQ       string `json:"qq"`
+		Email    string `json:"email"`
+	}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		utils.Fail(c, 400, "参数错误")
+		return
+	}
+}
--- middleware/jwt.go
+++ middleware/jwt.go
@@ -16,11 +16,15 @@ func JWTAuth() gin.HandlerFunc {
 			c.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "未登录"})
 			c.Abort()
 			return
-		} //判断是否登录
+		}
+		//判断是否登录
 
-		tokenStr := strings.TrimPrefix(header, "Bearer") //剥去Bearer前缀
+		tokenStr := strings.TrimPrefix(header, "Bearer")
+		//剥去Bearer前缀，因为前端传来的token中含Bearer但是jwt.Parse只认字符串
 		token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
 			return []byte(config.JWTSecret), nil
+			//tokenStr为前端传来的，func告诉库用哪吧密钥签名，jwt.Parse会自动告诉你用什么签名算法  注意；func内的nil不是外面err的值
+			//token为验完后返回的结构体
 
 		})
 		if err != nil || !token.Valid {
@@ -28,6 +32,7 @@ func JWTAuth() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+		//Valid代表是否有效,由jwt. Parse解析得到
 		c.Next()
 	}
 
