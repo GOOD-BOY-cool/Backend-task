606d68400709a24334fede1d44629ce0178f9095
GOOD-BOY-cool <zjzxh123.zj@outlook.com>
2026-09-14 17:14:51 +0800

    4

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"backend/config"
+	"backend/models"
 	"backend/utils"
 
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/gorm"
 )
 
@@ -23,4 +28,47 @@ func Register(c *gin.Context) {
 		utils.Fail(c, 400, "参数错误")
 		return
 	}
+	hash, _ := utils.HashPassword(body.Password)
+	user := models.User{
+		Account:      body.Account,
+		PasswordHash: hash,
+		QQ:           body.QQ,
+		Email:        body.Email,
+	}
+	if err := DB.Create(&user).Error; err != nil {
+		utils.Fail(c, 500, "注册失败账号已经存在")
+		return
+	}
+	utils.Success(c, nil)
+
+}
+func Login(c *gin.Context) {
+	var body struct {
+		Account  string `json:"accont"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		utils.Fail(c, 400, "参数错误")
+		return
+	}
+	var user models.User
+	if err := DB.Where("account=?", body.Account).First(&user).Error; err != nil {
+		utils.Fail(c, 401, "账号或密码错误")
+		return
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"role":    user.Role,
+		"exp":     time.Now().Add(time.Second * config.JWTExpire).Unix(),
+	})
+	tokenStr, _ := token.SignedString([]byte(config.JWTSecret))
+
+	utils.Success(c, gin.H{
+		"token": tokenStr,
+		"user": gin.H{
+			"level": user.Level,
+			"role":  user.Role,
+		},
+	})
+
 }
