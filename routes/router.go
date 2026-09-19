890643ede2297322182a42ca2e4ce2142509f184
GOOD-BOY-cool <zjzxh123.zj@outlook.com>
2026-09-19 20:59:47 +0800

    增加了与商品有关的功能，还有自我注解

--- routes/router.go
+++ routes/router.go
@@ -10,6 +10,7 @@ import (
 // *gin.Engine 和路由有关的结构体内部感觉很复杂
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Static("/uploads", "./uploads")
 	//gin.Default()=gin.New()(裸引擎) + Logger()(打印请求日志) + Recovery()（保证panic不崩溃）  gin.New()（返回一个空的gin.Engine结构体），Logger()和Recovery()是gin的中间件
 	//recovery主要靠内部的defer + recover()和c.Abort()来保证panic不崩溃)
 	auth := r.Group("/api/auth")
