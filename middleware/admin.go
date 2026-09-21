e5d7b4d614e4aa61c780c2bcc82b7a0ab9ab4276
GOOD-BOY-cool <zjzxh123.zj@outlook.com>
2026-09-21 18:08:04 +0800

    feat :管理员权限验证

--- middleware/admin.go
+++ middleware/admin.go
@@ -1,13 +1,30 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/controllers"
+	"backend/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func AdminAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		userID, exists := c.Get("user_id")
-		if condition {
-
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": 401, "message": "未登录"})
+			c.Abort()
+			return
 		}
+		var user models.User
+		controllers.DB.First(&user, userID)
+		if user.Role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{"code": 403, "msg": "需要管理员权限"})
+			c.Abort()
+			return
+		}
+		c.Next()
 
 	}
 }
